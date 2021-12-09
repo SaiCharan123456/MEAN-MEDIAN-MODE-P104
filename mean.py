55ee9d46a214deb44b2734db923d11b25ca7d4b4
SaiCharan123456 <77579506+SaiCharan123456@users.noreply.github.com>
2021-12-09 19:58:09 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,27 @@
+# Python program to print
+# mean of elements
+
+# list of elements to calculate mean
+import csv
+with open('MEAN,MEDIAN MODE-P104/height-weight.csv', newline='') as f:
+    reader = csv.reader(f)
+ 
+    file_data = list(reader)
+ 
+file_data.pop(0)
+
+new_data=[]
+for i in range(len(file_data)):
+	n_num = file_data[i][2]
+	new_data.append(float(n_num))
+
+
+
+n = len(new_data)
+total =0
+for x in new_data:
+    total += x
+
+mean = total / n
+
+print("Mean / Average is: " + str(mean))
